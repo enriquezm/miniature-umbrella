d5023444ff65c070bdb91318d1c2b6856138797c
Myles Enriquez <enri.myles@gmail.com>
2021-08-08 17:54:10 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const uid = require('./uid');
+const adjs = require('./adjs');
+const nouns = require('./nouns');
+
+function getRandomIndex(sizeOfArray) {
+  return Math.floor(Math.random() * sizeOfArray);
+}
+
+function convertToCapitalCase(stringToConvert) {
+  const upper = stringToConvert.toLowerCase().charAt(0).toUpperCase();
+  const remain = stringToConvert.slice(1);
+
+  return upper + remain;
+}
+
+const first = adjs[getRandomIndex(adjs.length)];
+const last = convertToCapitalCase(nouns[getRandomIndex(nouns.length)]);
+const generatedUid = uid(140);
+
+
+console.log(`New user generated: ${first}${last}`);
+console.log(`With mini_uID: ${generatedUid.slice(0,6)}`);
+console.log(`long_uID: ${generatedUid}`);
+
+// console.log(nouns.includes('night'));
